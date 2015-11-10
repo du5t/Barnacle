576d619ac571b15a482ca8cfbe32085dac21e235
dust <du5t@multiplexed.be>
2015-11-10 10:40:25 -0800

    initial commit: mucking about with toolbar

---
+++ index.android.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+var SwitchAndroid = require('SwitchAndroid');
+var ToolbarAndroid = require('ToolbarAndroid');
+
+var Barnacle = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <ToolbarAndroid
+           actions={toolbarActions}
+           navIcon={require('./assets/img/ic_menu_black_24dp.png')}
+           // onActionSelected={this._onActionSelected}
+           onIconClicked={() => this.setState({actionText: 'Icon clicked'})}
+          style={styles.toolbar}
+          // subtitle={this.state.actionText}
+          title="Barnacle" />
+      
+        <Text style={styles.instructions}>
+          URL bar
+        </Text>
+      
+        <Text style={styles.instructions}>
+          Post input area
+        </Text>
+      
+        <Text style={styles.instructions}>
+          Posts area
+          <Text style={styles.instructions}>
+            A single post
+            Should be clickable, navigating to its own thread
+          </Text>
+        </Text>
+      </View>
+    );
+  }
+});
+
+var toolbarActions = [
+  {title: 'Create', icon: require('./assets/img/ic_create_black_48dp.png'), show: 'always'},
+  {title: 'Filter'},
+  {title: 'Settings', icon: require('./assets/img/ic_settings_black_48dp.png'), show: 'always'},
+];
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  toolbar: {
+    flex: 1,
+    backgroundColor: '#e9eaed',
+    height: 56,
+  },
+});
+
+AppRegistry.registerComponent('Barnacle', () => Barnacle);
